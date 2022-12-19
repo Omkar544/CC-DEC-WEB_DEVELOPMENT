a8d1553179928f48a53682f759159993280843b9
Omkar Gore <ogore3414@gmail.com>
2022-12-19 21:43:55 +0530

    Initial commit

---
+++ Task1-Porfoilio_Website/README.md
@@ -0,0 +1,1 @@
+# C-odeClause_Omkar_portfolio
---
+++ Task1-Porfoilio_Website/index.php
@@ -0,0 +1,35 @@
+<?php include 'header.php'; ?>
+
+<!-- css -->
+
+
+
+<div class="container">
+    <?php include 'sidebar.php'; ?>
+    <!-- <div class="main">
+        <div class="menus">
+            <img class="menu" src="images/menu.png" alt="menu" width="25px">
+            <img class="close" src="images/close.png" alt="close" width="25px">
+        </div> -->
+    <div class="info-container">
+        <div class="dev-info">
+            <div class="hello">Hello , I am</div>
+            <b><div class="name">Omkar Gore</div><br></b>
+            <br><div class="about">Student, Developer, Learner, Intern</div><br>
+            <div class="moreabout">I am student of Sharad Institute Of Poltechnic Yadrav-Ichalakranji.
+               <br> I like to learn new things.
+                <br>I am also interested in to create a static websites.....<br>
+            </div>
+            <div class="btns">
+                <a href="cv.txt" target="_blank"><button>Download CV</button></a>
+                <a href="contact.php"><button>Contact Me</button></a>
+            </div>
+        </div>
+        <div class="dev-pic">
+            <img src="images/profile-photo.jpg">
+        </div>
+    </div>
+</div>
+</div>
+
+<?php include 'footer.php'; ?>
